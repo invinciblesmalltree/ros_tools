c7c275ef220056e08fa54749964e549191f9d5a6
invinciblesmalltree <2581283397@qq.com>
2024-05-25 16:28:21 +0800

    update

--- src/ros_tools/scripts/d435_node.py
+++ src/ros_tools/scripts/d435_node.py
@@ -15,8 +15,8 @@ def main():
 
     pipeline = rs.pipeline()
     config = rs.config()
-    config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
-    config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+    config.enable_stream(rs.stream.color, 1280, 720, rs.format.bgr8, 30)
+    config.enable_stream(rs.stream.depth, 1280, 720, rs.format.z16, 30)
 
     pipeline.start(config)
 
